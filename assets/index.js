18dcd45e6aabd04a6052bdecd782cfb95679c8fb
PapryczkQ12 <pstryczek41@gmail.com>
2025-03-30 13:24:26 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -26,7 +26,7 @@ imageInput.addEventListener('change', async () => {
     const response = await fetch('https://api.imgur.com/3/image', {
       method: 'POST',
       headers: {
-        Authorization: 'Client-ID c8c28d402435402',
+        Authorization: 'Client-ID 0b970a1b7896a69',
       },
       body: formData,
     });
